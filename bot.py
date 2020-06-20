fb39b750c9b4c39e247cd273f729f860b194bb73
Evgeny Petrov <groosha@protonmail.com>
2020-06-20 19:51:43 +0300

    Extended help message, /start handler

--- bot.py
+++ bot.py
@@ -22,8 +22,18 @@ dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
 
 
+@dp.message_handler(lambda message: message.chat.type == "private", commands="start")
+async def cmd_start(message: types.Message):
+    """
+    /start command handler for private chats
+    :param message: Telegram message with "/start" command
+    """
+    await message.answer(f"Your Telegram ID is <code>{message.chat.id}</code>\nHelp and source code: /help")
+    logs.track("/start")
+
+
 @dp.message_handler(commands="id")
-async def just_tell_id(message: types.Message):
+async def cmd_id(message: types.Message):
     """
     /id command handler for all chats
     :param message: Telegram message with "/id" command
@@ -38,7 +48,12 @@ async def show_help(message: types.Message):
     /help command handler for all chats
     :param message: Telegram message with "/help" command
     """
-    await message.answer('Use this bot to get ID for different entities across Telegram. '
+    await message.answer('Use this bot to get ID for different entities across Telegram:\n'
+                         '• Forward message from channel to get channel ID;\n'
+                         '• Forward message from user to get their ID (unless they restrict from doing so);\n'
+                         '• Send a sticker to get its file_id (currently you can use the sticker\'s file_id with any bot);\n'
+                         '• Add bot to group to get its ID (it will even tell you when you migrate from group to supergroup);\n'
+                         '• Use inline mode to send your Telegram ID to any chat.\n\n'
                          'Source code: https://github.com/MasterGroosha/my-id-bot.')
     logs.track("/help")
 
