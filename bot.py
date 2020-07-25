a7572c619505a6eb6cb3329c8a1264a88d4fbff6
Evgeny Petrov <groosha@protonmail.com>
2020-07-25 13:14:50 +0300

    Fixed missing link to anonymous forward description.

--- bot.py
+++ bot.py
@@ -93,8 +93,8 @@ async def get_user_id_with_privacy(message: types.Message):
     Handler for message forwarded from other user who hides their ID
     :param message: Telegram message with "forward_sender_name" field not empty
     """
-    msg = f"This user decided to <b>hide</b> their ID.\n\nLearn more about this feature "
-    f"<a href=\"https://telegram.org/blog/unsend-privacy-emoji#anonymous-forwarding\">here</a>."
+    msg = f"This user decided to <b>hide</b> their ID.\n\nLearn more about this feature " \
+        f"<a href=\"https://telegram.org/blog/unsend-privacy-emoji#anonymous-forwarding\">here</a>."
     if message.sticker:
         msg += f"\n\nAlso this sticker's ID is <code>{message.sticker.file_id}</code>"
     await message.reply(msg)
