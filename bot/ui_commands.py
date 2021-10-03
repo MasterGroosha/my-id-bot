489057a93f1977fd09ae21ef54a784a2c5a1ae95
Evgeny Petrov <groosha@protonmail.com>
2021-10-03 21:58:32 +0300

    Removed unused import

--- bot/ui_commands.py
+++ bot/ui_commands.py
@@ -1,5 +1,5 @@
 from aiogram import Bot
-from aiogram.types import BotCommand, BotCommandScopeAllPrivateChats, BotCommandScopeAllGroupChats, BotCommandScopeChat
+from aiogram.types import BotCommand, BotCommandScopeAllPrivateChats, BotCommandScopeAllGroupChats
 
 
 async def set_bot_commands(bot: Bot):
