5b67a9eeb03d6712d727e003bdae9971b1bc64a3
Groosha <groosha@protonmail.com>
2023-03-19 11:41:38 +0800

    Added FluentDispenser class to deal with localizations

---
+++ bot/fluent_helper.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+from fluent.runtime import FluentLocalization, FluentResourceLoader
+
+
+class FluentDispenser:
+    def __init__(self, locales_dir: Path, default_language: str = "en"):
+        self.__loader = FluentResourceLoader(str(locales_dir) + "/{locale}")
+        self.__default_language = default_language
+        self.languages = dict()
+
+        dirs_names = set()
+        default_language_dir = None
+        for item in locales_dir.iterdir():
+            dirs_names.add(item.name)
+            if item.name == self.__default_language:
+                default_language_dir = item
+
+        if not default_language_dir:
+            raise ValueError("FluentDispenser: default language directory not found")
+
+        ftl_files_list = [item.name for item in default_language_dir.iterdir() if item.suffix == ".ftl"]
+
+        for name in dirs_names:
+            if name == default_language:
+                self.languages[name] = FluentLocalization([self.__default_language], ftl_files_list, self.__loader)
+            else:
+                self.languages[name] = FluentLocalization([name, self.__default_language], ftl_files_list, self.__loader)
+
+    @property
+    def default_locale(self):
+        return self.languages[self.__default_language]
+
+    def get_language(self, language_code: str):
+        return self.languages.get(language_code, self.__default_language)
